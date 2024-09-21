8807034702bf2733b074165bbceb5738d1f308b6
kerr <bottom_in2132@hotmail.com>
2024-09-21 10:39:23 +0800

    建立

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 from datetime import datetime
+from scrape import scrape_stocks, scrape_pm25, get_pm25_json
+import json
 
 # print(__name__)
 
@@ -22,46 +24,100 @@ books = {
         "image_url": "https://im1.book.com.tw/image/getImage?i=https://www.books.com.tw/img/001/036/04/0010360466.jpg&v=62d695bak&w=348&h=348",
     },
     4: {
-        "name": "水豚",
-        "price": 899,
-        "image_url": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ3yr7DdhJ5e8WymXhtf1r4dbOHzJnRcaImqw&s",
+        "name": "飼養柯基教學",
+        "price": 1499,
+        "image_url": "https://cdn2-digiphoto.techbang.com/system/excerpt_images/9207/front/6470c572e810c05e1da083439550f5a2.jpg?1479452813",
     },
 }
 
 
-@app.route("/bmi/name=<name>&height=<h>&weight=<w>")
-def my_bmi(name, h, w):
+@app.route("/pm25-chart")
+def pm25_chart():
+    return render_template("pm25-chart.html")
+
+
+@app.route("/pm25-data")
+def pm25_data():
     try:
-        BMI = round(eval(w) / ((eval(h) / 100) ** 2), 2)
-        return f"<h1><span style='color:blue'>{name}</span> BMI:{BMI}</h1>"
+        json_data = get_pm25_json()
+        return json.dumps(json_data, ensure_ascii=False)
     except Exception as e:
         print(e)
+        return json.dumps({"result": "failure", "exception": str(e)})
+
+
+@app.route("/pm25", methods=["GET", "POST"])
+def get_pm25():
+    # GET
+    print(request.args)
+    # POST
+    print(request.form)
+    today = datetime.now()
+
+    sort = False
+    ascending = True
+
+    if request.method == "POST":
+        # 判斷是否按下排序按鈕
+        if "sort" in request.form:
+            sort = True
+            # 取得select的option
+            ascending = True if request.form.get("sort") == "true" else False
+
+    columns, values = scrape_pm25(sort, ascending)
+    data = {
+        "columns": columns,
+        "values": values,
+        "today": today.strftime("%Y/%m/%d %H:%M:%S"),
+    }
+    return render_template("pm25.html", data=data)
+
+
+@app.route("/stocks")
+def get_stocks():
+    datas = scrape_stocks()
+    return render_template("stocks.html", stocks=datas)
+
+
+@app.route("/bmi/name=<name>&height=<h>&weight=<w>")
+def get_bmi(name, h, w):
+    try:
+        bmi = round(eval(w) / (eval(h) / 100) ** 2, 2)
+        return f"<h1><span style='color:blue'>{name}</span> BMI:{bmi}<h1>"
+    except Exception as e:
+        print(e)
+
     return "<h2>參數不正確!</h2>"
 
 
 @app.route("/sum/x=<x>&y=<y>")
 def my_sum(x, y):
+    # 參數不正確，請輸出參數錯誤 (try + except)
     try:
         total = eval(x) + eval(y)
         return f"<h1>{x}+{y}={total}</h1>"
     except Exception as e:
         print(e)
+
     return "<h2>參數不正確!</h2>"
 
 
 @app.route("/book/<int:id>")
 def show_book(id):
+    # 輸出有書 <h1>第一本書:xxx</h1> 或 無此編號
     if id not in books:
         return f"<h2 style='color:red'>無此編號:{id}</h2>"
+
     return f"<h1>第{id}本書:{books[id]}</h1>"
 
 
 @app.route("/books")
 def show_books():
     print(books)
+
     for key in books:
         print(books[key])
-    # return books
+
     return render_template("books.html", books=books)
 
 
@@ -69,8 +125,7 @@ def show_books():
 def index():
     today = datetime.now()
     print(today)
-    # return f"<h1>Hello Flask!<br>{today}</h1>"
-    name = "Kerr"
+    name = "jerry"
     return render_template("index.html", date=today, name=name)
 
 
---
+++ scrape.py
@@ -0,0 +1,79 @@
+import requests
+from bs4 import BeautifulSoup
+import pandas as pd
+
+url = "https://data.moenv.gov.tw/api/v2/aqx_p_02?api_key=e8dd42e6-9b8b-43f8-991e-b3dee723a52d&limit=1000&sort=datacreationdate%20desc&format=JSON"
+six_countys = ["", , , , , , ]
+
+
+def get_six_pm25_json():
+    columns, values = scrape_six_pm25():
+    json_data = {"site":columns, "pm25":values}
+
+    return json_data
+
+
+def get_pm25_json():
+    columns, values = scrape_pm25()
+    
+    xdata = [value[0] for value in values]
+    ydata = [value[2] for value in values]
+    
+    json_data = {"site":xdata, "pm25":ydata}
+
+    return json_data
+
+def convert_value(value):
+    try:
+        return eval(value)
+    except:
+        return None
+    
+def get_pm25_data():
+    datas = request.get(url).json()["records"]
+    df = pd.DataFrame(datas)
+
+    df["pm25"] = df["pm25"].apply(convert_value)
+
+    df=dropna()
+
+    return df
+
+def scrape_six_pm25():
+    try:
+        df = get_pm25
+        columns = df.columns
+        values = df.values
+
+        return columns, values
+    except Exception as e:
+        print(e)
+
+    return None, 404
+
+def scrape_stocks():
+    url = "https://histock.tw/index"
+    try:
+        resp = requests.get(url)
+        soup = BeautifulSoup(resp.text, "lxml")
+        trs = soup.find(string="加權指數").find_parent("div").find_all("tr")
+        datas = []
+        for tr in trs:
+            data = []
+            for th in tr.find_all("th"):
+                data.append(th.text.strip())
+            for td in tr.find_all("td"):
+                data.append(td.text.strip())
+            datas.append(data)
+
+        return datas
+    except Exception as e:
+        print(e)
+
+    return None
+
+
+if __name__ == "__main__":
+    # print(scrape_stocks())
+    print(get_pm25_json())
+
